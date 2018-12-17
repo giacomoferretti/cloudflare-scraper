4e3fa3f85022b013065b86fedcd5e641106a1be1
hexile <giacomo.ferretti.00@gmail.com>
2018-12-17 22:53:52 +0100

    Initial commit

--- src/main/java/xyz/hexile/cloudflarescraper/CloudflareScraper.java
+++ src/main/java/xyz/hexile/cloudflarescraper/CloudflareScraper.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Giacomo Ferretti
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package xyz.hexile.cloudflarescraper;
 
 import org.apache.commons.io.IOUtils;
@@ -77,10 +93,12 @@ public class CloudflareScraper {
     }
 
     public CloudflareScraper(Builder builder) {
+        System.out.println(System.getProperty("https.protocols"));
         this.uri = builder.uri;
         this.challengeDelay = builder.challengeDelay;
         this.connectionTimeout = builder.connectionTimeout;
         this.readTimeout = builder.readTimeout;
+        System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2");
     }
 
     public static class Builder {
